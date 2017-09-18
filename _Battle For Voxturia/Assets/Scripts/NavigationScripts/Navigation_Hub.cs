abc465e0b7ca23224fdf6646057dd0b905933fef
opachot <opachot@hotmail.com>
2017-09-17 22:35:40 -0400

    Started the Unity project. + Added basic MockUp for TitleScreen and Hub, but the hub still need the pop-up and button redirection.

---
+++ _Battle For Voxturia/Assets/Scripts/NavigationScripts/Navigation_Hub.cs
@@ -0,0 +1,64 @@
+﻿/*
+Company: Voxturia Game
+Author:  Sébastien Godbout
+Date:    17 September 2017
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Navigation_Hub : MonoBehaviour {
+
+	#region DECLARATION
+    // CONST
+
+    // PRIVATE
+
+    // PUBLIC
+
+    #endregion
+    
+	#region UNITY METHODE
+	void Awake() {
+		
+    }
+	
+	void Start() {
+        
+	}
+	
+	void Update() {
+		
+	}
+    #endregion
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+    public void NavigateTo_TeamList() {
+        SceneManager.LoadScene("TeamList");
+    }
+
+    public void NavigateTo_Shop() {
+        SceneManager.LoadScene("Shop");
+    }
+
+    public void NavigateTo_Option() {
+        SceneManager.LoadScene("Option");
+    }
+
+    public void NavigateTo_Map() {
+        SceneManager.LoadScene("Map");
+    }
+
+    public void NavigateTo_Items() {
+        SceneManager.LoadScene("Items");
+    }
+
+    public void NavigateTo_Help() {
+        SceneManager.LoadScene("Help");
+    }
+}
---
+++ _Battle For Voxturia/Assets/Scripts/NavigationScripts/Navigation_TitleScreen.cs
@@ -0,0 +1,44 @@
+﻿/*
+Company: Voxturia Game
+Author:  Sébastien Godbout
+Date:    17 September 2017
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Navigation_TitleScreen : MonoBehaviour {
+
+    #region DECLARATION
+    // CONST
+
+    // PRIVATE
+
+    // PUBLIC
+
+    #endregion
+    
+    #region UNITY METHODE
+    void Awake() {
+
+    }
+
+    void Start() {
+		
+	}
+	
+	void Update() {
+		
+	}
+    #endregion
+
+    public void NavigateTo_Hub() {
+        SceneManager.LoadScene("Hub");
+    }
+    
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
