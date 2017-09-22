e8d72c7381b09a93f4784add4ca24bc523548da2
opachot <opachot@hotmail.com>
2017-09-21 20:29:50 -0400

    Worked on the TeamList screen (TeamCreation pop-up and error pop-up)

--- _Battle For Voxturia/Assets/Scripts/UiScript/Buttons_TeamList.cs
+++ _Battle For Voxturia/Assets/Scripts/UiScript/Buttons_TeamList.cs
@@ -13,12 +13,20 @@ public class Buttons_TeamList : MonoBehaviour {
 
     #region DECLARATION
     // CONST
+    private const int MIN_TEAM_NAME_LENGTH = 3;
 
     // PRIVATE
     private Navigation navigation;
 
     // PUBLIC
-    public GameObject CreateNewTeam_PopUp;
+    public GameObject createNewTeam_popUp;
+    public GameObject error_popUp;
+
+    public InputField newTeamName_inputField;
+
+    public Button createTeam_btn;
+
+    public Text errorText;
 
     #endregion
 
@@ -28,7 +36,7 @@ public class Buttons_TeamList : MonoBehaviour {
     }
 	
 	void Start() {
-		
+        
 	}
 	
 	void Update() {
@@ -39,7 +47,7 @@ public class Buttons_TeamList : MonoBehaviour {
 
     #region Default buttons
     public void CreateNewTeamButton() {
-        CreateNewTeam_PopUp.SetActive(true);
+        createNewTeam_popUp.SetActive(true);
     }
 
     public void ReturnButton() {
@@ -49,12 +57,82 @@ public class Buttons_TeamList : MonoBehaviour {
 
     #region CreateNewTeam popUp buttons
     public void CreateTeamButton() {
-        // TODO: Save the creation of the team.
-        CreateNewTeam_PopUp.SetActive(false);
+        string newName      = newTeamName_inputField.text;
+        bool   isValidName = ValidateName(newName);
+
+        if(isValidName) {
+            // TODO: Save the creation of the team and reload the teamList board.
+
+            // Reset the popUp.
+            newTeamName_inputField.text = "";
+            createTeam_btn.interactable = false;
+
+            createNewTeam_popUp.SetActive(false);
+        }
     }
 
     public void CancelCreationButton() {
-        CreateNewTeam_PopUp.SetActive(false);
+        // Reset the popUp.
+        newTeamName_inputField.text = "";
+        createTeam_btn.interactable = false;
+
+        createNewTeam_popUp.SetActive(false);
+    }
+    #endregion
+
+    #region Error popUp buttons
+    public void ErrorOkButton() {
+        error_popUp.SetActive(false);
+        errorText.text = "Unknown Error";
+    }
+    #endregion
+
+
+    public void OnInputFieldValueChanged() {
+        UpdateCreateTeamButton();
+    }
+
+    private void UpdateCreateTeamButton() {
+        bool isValidNameLength = newTeamName_inputField.text.Length < MIN_TEAM_NAME_LENGTH;
+
+        if(isValidNameLength) {
+            createTeam_btn.interactable = false;
+        }
+        else {
+            createTeam_btn.interactable = true;
+        }
+    }
+
+    #region Error validation
+    private bool ValidateName(string newName) {
+        bool isValidName = true;
+
+        if(!IsAvailableName()) {
+            TrowError("Error: The name is already taken");
+            isValidName = false;
+        }
+        else if(!IsMoralName()) {
+            TrowError("Error: The name is inappropriate");
+            isValidName = false;
+        }
+
+        return isValidName;
+    }
+
+    private bool IsAvailableName() {
+        // TODO
+        return true;
+    }
+
+    private bool IsMoralName() {
+        // TODO
+        return true;
+    }
+
+
+    private void TrowError(string error) {
+        errorText.text = error;
+        error_popUp.SetActive(true);
     }
     #endregion
 }
