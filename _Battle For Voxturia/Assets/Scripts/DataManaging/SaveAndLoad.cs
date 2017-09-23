38c6890ed9f25884e2222e14d0dd5fbada27f587
opachot <opachot@hotmail.com>
2017-09-23 04:28:13 -0400

    Planned a realy basic layout to save information on the computer.

---
+++ _Battle For Voxturia/Assets/Scripts/DataManaging/SaveAndLoad.cs
@@ -0,0 +1,84 @@
+﻿/*
+Company: Voxturia Game
+Author:  Sébastien Godbout
+Date:    23 September 2017
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+/***************************
+ *          -NOTE-
+ * Will need to acces all the thing that will need to be saved.
+ * ************************/
+
+public class SaveAndLoad : MonoBehaviour {
+
+	#region DECLARATION
+    // CONST
+
+    // PRIVATE
+
+    // PUBLIC
+
+    #endregion
+
+	#region UNITY METHODE
+	void Awake() {
+        print("Application.persistentDataPath: " + Application.persistentDataPath);
+    }
+	
+	void Start() {
+		
+	}
+	
+	void Update() {
+		
+	}
+    #endregion
+
+
+    public void Save() {
+        BinaryFormatter bf   = new BinaryFormatter();
+        FileStream      file = File.Create(Application.persistentDataPath + "/teamsInfo.dat");
+
+        SerialisableTeamsInfo teamsData = new SerialisableTeamsInfo();
+
+        //--------------------testing values-----------------------
+        // Need to encrypt and only pass a string
+
+        const int NB_TEAM = 3;
+        teamsData.teamsNames = new string[NB_TEAM];
+        string[] myTeamsNames = new string[] {"TrolololTeam",
+                                              "xyzTeam"     ,
+                                              "metaTeam"    };
+
+        for(int i = 0; i < NB_TEAM; i++) {
+            teamsData.teamsNames[i] = myTeamsNames[i];
+        }
+        //---------------------------------------------------------
+
+        bf.Serialize(file, teamsData);
+        file.Close();
+
+        print("Saving completed!");
+        print("Saved teams names: " + teamsData.teamsNames[0] + " - " + teamsData.teamsNames[1] + " - " + teamsData.teamsNames[2]);
+    }
+
+    public void Load() {
+        if(File.Exists(Application.persistentDataPath + "/teamsInfo.dat")) {
+            BinaryFormatter bf   = new BinaryFormatter();
+            FileStream      file = File.Open(Application.persistentDataPath + "/teamsInfo.dat", FileMode.Open);
+
+            SerialisableTeamsInfo teamsData = (SerialisableTeamsInfo)bf.Deserialize(file);
+            file.Close();
+            // Need to decript
+
+            print("Loading completed!");
+            print("Loaded teams names: " + teamsData.teamsNames[0] + " - " + teamsData.teamsNames[1] + " - " + teamsData.teamsNames[2]);
+        }
+    }
+}
---
+++ _Battle For Voxturia/Assets/Scripts/DataManaging/SerialisableTeamsInfo.cs
@@ -0,0 +1,37 @@
+﻿/*
+Company: Voxturia Game
+Author:  Sébastien Godbout
+Date:    23 September 2017
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+/***************************
+ *          -NOTE-
+ * Save teams name, levels and other team stats.
+ * Save teams character, they skills and they equipment and lvl, etc...
+ * Save character reserve list.
+ * Save discovered items/budget
+ * Save map/quest progression
+ * Save option
+ * ************************/
+
+[Serializable]
+public class SerialisableTeamsInfo {
+
+    #region DECLARATION
+    // CONST
+
+    // PRIVATE
+
+    // PUBLIC
+    public string[] teamsNames; 
+
+    #endregion
+
+
+	
+}
