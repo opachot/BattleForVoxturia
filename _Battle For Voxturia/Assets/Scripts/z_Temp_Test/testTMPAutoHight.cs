37242f0592a651f34ee6fbd5ef2a62d6851fb0ad
Sébastien Godbout <godboutsebastien@gmail.com>
2018-03-04 05:15:01 -0500

    - Started rework on the characterCustomisation screen on the selected section (using scroll view + TMP instead of static text).
    - Next task is to finish what was started on the characterCustomisation screen since the screen is broke atm.

---
+++ _Battle For Voxturia/Assets/Scripts/z_Temp_Test/testTMPAutoHight.cs
@@ -0,0 +1,43 @@
+﻿/*
+Company: Voxturia Game
+Author:  Sébastien Godbout
+Date:    DAY MONTH YEARS
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class testTMPAutoHight : MonoBehaviour {
+
+	#region DECLARATION
+    // CONST
+
+    // PRIVATE
+
+    // PUBLIC
+    public TMP_Text myTxt;
+
+    #endregion
+
+	#region UNITY METHODE
+	void Awake() {
+		
+    }
+	
+	void Start() {
+	}
+	
+	void Update() {
+        if(Input.anyKey) {
+            // this work, set hight depending of that value.
+            Debug.Log(myTxt.textInfo.lineCount);
+
+            gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(gameObject.GetComponent<RectTransform>().sizeDelta.x,myTxt.textInfo.lineCount*13);
+        }
+	}
+    #endregion
+
+}
